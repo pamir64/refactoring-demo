b32744c8c4e902902d36428cf3ddfb7c4f9d2def
Pavel Mironenko <pmironenko@gmail.com>
2016-02-08 15:02:40 +0300

    more statement tests; - PIT

--- src/test/java/com/scrumtrek/simplestore/test/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/test/CustomerTest.java
@@ -1,5 +1,6 @@
 package com.scrumtrek.simplestore.test;
 
+import com.google.common.base.Preconditions;
 import com.scrumtrek.simplestore.Customer;
 import com.scrumtrek.simplestore.Movie;
 import com.scrumtrek.simplestore.PriceCodes;
@@ -12,9 +13,11 @@ public class CustomerTest {
 
     private Movie movie1;
     private Movie movie2;
+    private Movie movie3;
     private Customer customer;
     private Rental rental1;
     private Rental rental2;
+    private Rental rental3;
 
     @Before
     public void prepareTestData() {
@@ -22,18 +25,33 @@ public class CustomerTest {
 
         movie1 = new Movie("Movie1", PriceCodes.Regular);
         movie2 = new Movie("Movie2", PriceCodes.NewRelease);
+        movie3 = new Movie("Movie3", PriceCodes.Childrens);
 
         rental1 = new Rental(movie1, 3);
         rental2 = new Rental(movie2, 2);
-    }
+        rental3 = new Rental(movie3, 3);
 
-    private void prepareRentals() {
         customer.addRental(rental1);
         customer.addRental(rental2);
+        customer.addRental(rental3);
+    }
+
+    private void cleanRentals() {
+        customer = new Customer("Customer1");
+    }
+
+    private double getStatementTotalAmount(String statement) {
+        Preconditions.checkNotNull(statement);
+        String amountStr = "Amount owed is ";
+        int i = statement.indexOf(amountStr);
+        int ni = statement.indexOf("\n", i);
+
+        return Double.parseDouble(statement.substring(i + amountStr.length(), ni));
     }
 
     @Test
     public void statementZeroTest() {
+        cleanRentals();
         String statement = customer.Statement();
         System.out.println(statement);
         Assert.assertNotNull(statement);
@@ -42,21 +60,43 @@ public class CustomerTest {
 
     @Test
     public void statementTest() {
-        prepareRentals();
         String statement = customer.Statement();
         System.out.println(statement);
         Assert.assertNotNull(statement);
-        Assert.assertTrue(statement.contains("Amount owed is 9.5"));
+        Assert.assertTrue(statement.contains("Amount owed is 11.0"));
     }
 
     @Test
-    public void statementPriceCodeTest() {
-        prepareRentals();
-        movie2.setPriceCode(PriceCodes.Childrens);
-
+    public void priceCodeChangeTest() {
         String statement = customer.Statement();
-        System.out.println(statement);
-        Assert.assertNotNull(statement);
-        Assert.assertTrue(statement.contains("Amount owed is 5.0"));
+        final double amount1 = getStatementTotalAmount(statement);
+        movie1.setPriceCode(PriceCodes.Childrens);
+        statement = customer.Statement();
+        final double amount2 = getStatementTotalAmount(statement);
+        Assert.assertNotEquals(amount1, amount2);
+    }
+
+    private void rentalDaysTest(PriceCodes priceCode, int days, double expectedAmount) {
+        String statement = customer.Statement();
+        final double amount1 = getStatementTotalAmount(statement);
+        Movie movie = new Movie("extra Movie", priceCode);
+        Rental extraRental = new Rental(movie, days);
+        customer.addRental(extraRental);
+        statement = customer.Statement();
+        final double amount2 = getStatementTotalAmount(statement);
+        Assert.assertEquals(expectedAmount, amount2 - amount1, 0.0001);
+    }
+
+    @Test
+    public void regularDaysTest() {
+        rentalDaysTest(PriceCodes.Regular, 1, 2);
+        rentalDaysTest(PriceCodes.Regular, 2, 2);
+        rentalDaysTest(PriceCodes.Regular, 3, 3.5);
+        rentalDaysTest(PriceCodes.NewRelease, 2, 6);
+        rentalDaysTest(PriceCodes.NewRelease, 3, 9);
+        rentalDaysTest(PriceCodes.Childrens, 1, 1.5);
+        rentalDaysTest(PriceCodes.Childrens, 3, 1.5);
+        rentalDaysTest(PriceCodes.Childrens, 4, 1.5);
+        rentalDaysTest(PriceCodes.Childrens, 5, 3);
     }
 }
