ff19aa5a9f2ecb9353553a30e97e1787b9702cea
Pavel Mironenko <pmironenko@gmail.com>
2016-02-08 13:32:29 +0300

    more statement test + PIT

--- src/test/java/com/scrumtrek/simplestore/test/CustomerTest.java
+++ src/test/java/com/scrumtrek/simplestore/test/CustomerTest.java
@@ -13,13 +13,23 @@ public class CustomerTest {
     private Movie movie1;
     private Movie movie2;
     private Customer customer;
+    private Rental rental1;
+    private Rental rental2;
 
     @Before
     public void prepareTestData() {
+        customer = new Customer("Customer1");
+
         movie1 = new Movie("Movie1", PriceCodes.Regular);
         movie2 = new Movie("Movie2", PriceCodes.NewRelease);
 
-        customer = new Customer("Customer1");
+        rental1 = new Rental(movie1, 3);
+        rental2 = new Rental(movie2, 2);
+    }
+
+    private void prepareRentals() {
+        customer.addRental(rental1);
+        customer.addRental(rental2);
     }
 
     @Test
@@ -32,14 +42,21 @@ public class CustomerTest {
 
     @Test
     public void statementTest() {
-        Rental rental1 = new Rental(movie1, 3);
-        Rental rental2 = new Rental(movie2, 2);
-
-        customer.addRental(rental1);
-        customer.addRental(rental2);
+        prepareRentals();
         String statement = customer.Statement();
         System.out.println(statement);
         Assert.assertNotNull(statement);
         Assert.assertTrue(statement.contains("Amount owed is 9.5"));
+    }
+
+    @Test
+    public void statementPriceCodeTest() {
+        prepareRentals();
+        movie2.setPriceCode(PriceCodes.Childrens);
+
+        String statement = customer.Statement();
+        System.out.println(statement);
+        Assert.assertNotNull(statement);
+        Assert.assertTrue(statement.contains("Amount owed is 5.0"));
     }
 }
